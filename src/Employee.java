89c7381a209d6b7eaa787dacfe2a230a74522c4b
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2023-10-14 14:26:58 +0300

    New commit

---
+++ src/Employee.java
@@ -0,0 +1,89 @@
+public class Employee {
+
+
+    private String surname;
+    private String name;
+    private String patronymic;
+
+    private int department;
+    private double salary;
+    private static int counter = 1;
+    private int id;
+
+    public Employee(String surname, String name, String patronymic, int department, double salary) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public static void setCounter(int counter) {
+        Employee.counter = counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "ФИО сотрудника : " + surname + " " + name + " " + patronymic + " " + department + " " + salary + " " + counter + " " + id;
+    }
+
+    public String toStringFIO() {
+        return surname + " " + name + " " + patronymic;
+    }
+
+}
+
---
+++ src/Storage.java
@@ -0,0 +1,69 @@
+public class Storage {
+
+    private Employee[] employee;
+
+    public Storage() {
+        employee = new Employee[10];
+
+        employee[0] = new Employee("Достоевский", "Федор", "Михайлович", 1, 50_000);
+        employee[1] = new Employee("Лермонтов", "Михаил", "Юрьевич", 2, 52_000);
+        employee[2] = new Employee("Гоголь", "Николай", "Васильевич", 3, 51_000);
+        employee[3] = new Employee("Пешков", "Алексей", "Максимович", 4, 51_500);
+        employee[4] = new Employee("Фет", "Афанасий", "Афанасьевич", 5, 53_000);
+        employee[5] = new Employee("Есенин", "Сергей", "Александрович", 1, 700_000);
+        employee[6] = new Employee("Чехов", "Антон", "Павлович", 2, 55_000);
+        employee[7] = new Employee("Толстой", "Лев", "Николаевич", 3, 60_000);
+        employee[8] = new Employee("Булгаков", "Михаил", "Афанасьевич", 4, 65_000);
+        employee[9] = new Employee("Пушкин", "Александр", "Сергеевич", 5, 72_000);
+    }
+
+    public void getAllEmployeeDate() {
+        for (int i = 0; i < employee.length - 1; i++) {
+            System.out.println(employee);
+        }
+    }
+
+    public double findTotalMonthlySalary() {
+        double sum = 0;
+        for (Employee value : employee) {
+            sum += value.getSalary();
+        }
+        return sum;
+    }
+
+    public Employee findEmployeeMinSalary() {
+        Employee poorEmpl = employee[0];
+        for (Employee employee : employee) {
+            if (poorEmpl.getSalary() > employee.getSalary()) {
+                poorEmpl = employee;
+            }
+        }
+        return poorEmpl;
+    }
+
+    public Employee findEmployeeMaxSalary() {
+        Employee richEmpl = employee[0];
+        for (Employee employee : employee) {
+            if (richEmpl.getSalary() < employee.getSalary()) {
+                richEmpl = employee;
+            }
+        }
+        return richEmpl;
+
+    }
+
+    public double findTheAverageSalary() {
+        return  findTotalMonthlySalary() / employee.length;
+    }
+
+    public void printFIO() {
+        for (Employee employee : employee) {
+            System.out.println(employee.toString());
+        }
+    }
+}
+
+
+
+
+
